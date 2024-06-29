e67f07b1f3939bea6b3c3461dc78bf660e296681
Nik <gnezdilovnikitakonstantinovich@mail.ru>
2024-06-29 22:14:11 +0500

    End of the day 29.06.2024

---
+++ DoneTask.py
@@ -0,0 +1,21 @@
+# def name (a, b):
+#     return a + b
+
+
+# def is_even(n):
+#     if n % 2 == 0:
+#         print("True")
+#     else:
+#         print("False")
+# is_even(n = int(input("Enter a number: ")))
+
+# def say_hello(name):
+#     print(f"Hello, {name}")
+# say_hello("Tom")
+# say_hello("Bob")
+# say_hello("Alice")
+
+Округление до близайшего четного
+print(round(2.5))
+print(round(3.5))
+
