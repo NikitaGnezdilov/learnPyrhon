4c603464a1f2a244bc7e6e008597b5a05d0add43
Nik <gnezdilovnikitakonstantinovich@mail.ru>
2024-07-01 01:44:32 +0500

    End of the day(continuation) 30.06.2024

--- DoneTask.py
+++ DoneTask.py
@@ -26,30 +26,43 @@
 # print((lambda  a, b, c: round(a+b+c)/++3)(1, 2 ,3))
 
 # Прототип авторизации
+def main():
+    action = input("Registration or login? Enter: ")
+    def registration():
+        userName = input("Enter your name: ")
+        userSurname = input("Enter your surname: ")
+        userPassword = input("Enter your password: ")
+        userPasswordAgain = input("Enter your password again: ")
 
-action = input("Registration or login? Enter: ")
-def registration():
-    userName = input("Enter your name: ")
-    userSurname = input("Enter your surname: ")
-    userPassword = input("Enter your password: ")
-    userPasswordAgain = input("Enter your password again: ")
+        if userPassword == userPasswordAgain:
+            print("Welcome " + userName + "!")
+        else:
+            print("Sorry " + userName + "'s password does not match.")
 
-    if userPassword == userPasswordAgain:
-        print("Welcome " + userName + "!")
+    def authorizationUser ():
+        userName_aut = input("Enter your name: ")
+        userSurname_aut = input("Enter your surname: ")
+        userPassword_aut = input("Enter your password: ")
+        userName_saved = "Nikita"
+        userSurname_saved = "Gnezdilov"
+        userPassword_saved = "123"
+
+        if userName_aut == userName_saved and userSurname_aut == userSurname_saved and userPassword_aut == userPassword_saved:
+            print("Welcome " + userName_saved)
+        else:
+            print("You entered an incorrect first or last name or password. Sorry " + userName_aut)
+    if action == "Registration" or action == "registration":
+        registration()
+    elif action == "login" or action == "Login":
+        authorizationUser()
     else:
-        print("Sorry " + userName + "'s password does not match.")
+        print("Sorry " + action + " is not a valid action. You need enter registration or login.")
 
-def authorizationUser ():
-    userName_aut = input("Enter your name: ")
-    userSurname_aut = input("Enter your surname: ")
-    userPassword_aut = input("Enter your password: ")
-
-    print("Welcome " + userName_aut)
-
-if action == "Registration" or action == "registration":
-    registration()
-elif action == "login" or action == "Login":
-    authorizationUser()
-else:
-    print("Sorry " + action + " is not a valid action. You need enter registration or login.")
+def question():
+    ans = input("If you want to continue working on our website, please log in or authorize. (Y/N): ")
+    if ans == "Y" or ans == "y":
+        main()
+    else:
+        print("Thank you! See you later")
+question()
 
