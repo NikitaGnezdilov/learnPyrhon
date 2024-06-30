191cd1f7ae3e7ccff9a7604a173a686503b206c1
Nik <gnezdilovnikitakonstantinovich@mail.ru>
2024-07-01 01:15:20 +0500

    End of the day 30.06.2024

--- DoneTask.py
+++ DoneTask.py
@@ -24,3 +24,32 @@
 
 # Написать lambda-функцию, которая выводит среднее арифметическое 3 чисел.
 # print((lambda  a, b, c: round(a+b+c)/++3)(1, 2 ,3))
+
+# Прототип авторизации
+
+action = input("Registration or login? Enter: ")
+def registration():
+    userName = input("Enter your name: ")
+    userSurname = input("Enter your surname: ")
+    userPassword = input("Enter your password: ")
+    userPasswordAgain = input("Enter your password again: ")
+
+    if userPassword == userPasswordAgain:
+        print("Welcome " + userName + "!")
+    else:
+        print("Sorry " + userName + "'s password does not match.")
+
+def authorizationUser ():
+    userName_aut = input("Enter your name: ")
+    userSurname_aut = input("Enter your surname: ")
+    userPassword_aut = input("Enter your password: ")
+
+    print("Welcome " + userName_aut)
+
+if action == "Registration" or action == "registration":
+    registration()
+elif action == "login" or action == "Login":
+    authorizationUser()
+else:
+    print("Sorry " + action + " is not a valid action. You need enter registration or login.")
+
