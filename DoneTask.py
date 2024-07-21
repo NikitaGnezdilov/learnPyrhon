2cb60fe66611f251349b5a5c3cefd667a83633de
Nik <gnezdilovnikitakonstantinovich@mail.ru>
2024-07-21 23:15:42 +0500

    End of the day 21.07.2024

--- DoneTask.py
+++ DoneTask.py
@@ -2,6 +2,7 @@
 #     return a + b
 import math
 
+
 # def is_even(n):
 #     if n % 2 == 0:
 #         print("True")
@@ -118,3 +119,173 @@ import math
 # circle = Circle(2)
 # print(circle.area())
 # print(circle.circumference())
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.__name = name  # устанавливаем имя
+#         self.__age = age  # устанавливаем возраст
+#
+#     def print_person(self):
+#         print(f"Имя: {self.__name}\tВозраст: {self.__age}")
+#
+#
+# tom = Person("Tom", 39)
+# tom.__name = "Человек-паук"  # пытаемся изменить атрибут __name
+# tom.__age = -129  # пытаемся изменить атрибут __
+# # tom.print_person()  # Имя: Tom        Возраст: 39
+
+
+# class House:
+#     def __init__(self, number, age, street, house_type):
+#         self.number = number
+#         self.age = age
+#         self.street = street
+#         self.house_type = house_type
+#
+#     def print_house(self):
+#         print(
+#             f"Номер дома: {self.number}, улица дома: {self.street}, возраст дома: {self.age}, тип дома: {self.house_type}.")
+#
+#
+# firstHouse = House(1, 10, "Pushkina", "multi-storey")
+# secondHouse = House(2, 13, "Pushkina", "multi-storey")
+# thirdHouse = House(3, 12, "Pushkina", "multi-storey")
+# fourthHouse = House(4, 11, "Pushkina", "multi-storey")
+# firstHouse.print_house()
+# secondHouse.print_house()
+# thirdHouse.print_house()
+# fourthHouse.print_house()
+# """Данные больницы"""
+#
+#
+# class Hospital:
+#     """Инициализирует имя, адрес, город и состояние посетителя."""
+#
+#     def __init__(self, name, age, address, city, state):
+#         self.__name = name
+#         self.__age = age
+#         self.__address = address
+#         self.__city = city
+#         self.__state = state
+#
+#     """Выводит всю информацию в командную строку"""
+#
+#     def print_information(self):
+#         print(
+#             f"Name: {self.__name}, Age: {self.__age}, Address: {self.__address}, City: {self.__city}, State: {self.__state}")
+#
+#     """Сеттер для установления возраста"""
+#
+#     def set_age(self, age):
+#         if 0 < age < 100:
+#             self.__age = age
+#         else:
+#             print("Incorrect Age")
+#
+#     """Сеттер для установления адреса"""
+#
+#     def set_address(self, address_string):
+#         self.__address = address_string
+#
+#     """Сеттер для установления города проживания"""
+#
+#     def set_city(self, city_string):
+#         self.__city = city_string
+#
+#     """Сеттер для установления состояния посетителя"""
+#
+#     def set_state(self, state_string):
+#         self.__state = state_string
+#
+#     """Геттер для получения установленного возраста"""
+#
+#     def get_age(self):
+#         return self.__age
+#
+#     """Геттер для получения установленного адреса"""
+#
+#     def get_address(self):
+#         return self.__address
+#
+#     """Геттер для получения установленного города проживания посетителя больницы"""
+#
+#     def get_city(self):
+#         return self.__city
+#
+#     """Геттер для получения установленного состояние посетителя больницы"""
+#
+#     def get_state(self):
+#         return self.__state
+#
+#
+# tom = Hospital("Tom", 15, "Pushkina 4", "Moscow", "Sick")
+# tom.set_age(25)
+# tom.set_state("A little sick")
+# tom.set_city("Tombow")
+# tom.print_information()
+
+class Hospital:
+    """Инициализирует имя, адрес, город и состояние посетителя."""
+
+    def __init__(self, name, age, address, city, state):
+        self.__name = name
+        self.__age = age
+        self.__address = address
+        self.__city = city
+        self.__state = state
+
+    """Выводит всю информацию в командную строку"""
+
+    def print_information(self):
+        print(
+            f"Name: {self.__name}, Age: {self.__age}, Address: {self.__address}, City: {self.__city}, State: {self.__state}")
+
+    """Сеттер для установления возраста"""
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, age):
+        if 0 < age < 100:
+            self.__age = age
+        else:
+            print("Incorrect Age")
+
+    """Сеттер для установления адреса"""
+
+    @property
+    def address(self):
+        return self.__address
+
+    @address.setter
+    def address(self, address_string):
+        self.__address = address_string
+
+    """Сеттер для установления города проживания"""
+
+    @property
+    def city(self):
+        return self.__city
+
+    @city.setter
+    def city(self, city_string):
+        self.__city = city_string
+
+    """Сеттер для установления состояния посетителя"""
+
+    @property
+    def state(self):
+        return self.__state
+
+    @state.setter
+    def state(self, state_string):
+        self.__state = state_string
+
+
+tom = Hospital("Tom", 15, "Pushkina 4", "Moscow", "Sick")
+tom.age = 25
+tom.state = "A little sick"
+tom.city = "Tombow"
+tom.print_information()
